513d6a3724ebc1fed0592fe35ecacca444b21e44
bestyize <3011429848@qq.com>
2025-07-19 19:12:19 +0800

    update latest

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -49,6 +49,8 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material3)
             implementation(compose.ui)
+            implementation(libs.uihandler)
+            implementation(compose.materialIconsExtended)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.ktor)
